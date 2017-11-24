dc6765e9761b9246fc3152ecf65b062c71a1bb3e
Lee Helpingstine <sig11@reprehensible.net>
2017-11-24 14:53:45 +0000

    CMake updates and read delay calculation
    CMake minimum version bump to 3.0 for C++11 options
    Automatically using system threading library
    minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 #include<fstream>
 #include<string>
 #include<sstream>
@@ -8,16 +9,18 @@
 #include<mutex>
 #include<condition_variable>
 
-const std::string sensor_path = "/sys/bus/w1/devices/28-000008aac4f7/w1_slave";
+const std::string sensor_path = "/sys/devices/w1_bus_master1/28-000008aac4f7/w1_slave";
+const std::chrono::seconds read_frequency(3);
 
 std::mutex m;
 std::condition_variable cv;
 int temp = 0;
 
+// Looks like  ~900ms to read the device...
 int read_sensor(const std::string& path) {
 	std::ifstream sensor(path);
 	if(!sensor.is_open()) {
-		std::cout << "Can't open sensor file.\n";
+		std::cout << "Can't open sensor device.\n";
 		exit(-1);
 	}
 
@@ -25,11 +28,11 @@ int read_sensor(const std::string& path) {
 	std::regex temp_regex("t=([0-9]*)$");
 	std::smatch temp_match;
 	while(std::getline(sensor, line)) {
+		// TODO: Check CRC instead of discarding the first line.
 		if(std::regex_search(line, temp_match, temp_regex)) {
 			return std::stoi(temp_match[1]);
-			break;
-		}	
-	}	
+		}
+	}
 
 	std::cout << "Failed to read temperature.\n";
 	return -1;
@@ -38,9 +41,21 @@ int read_sensor(const std::string& path) {
 void poll_sensor() {
 	while(1) {
 		std::unique_lock<std::mutex> lock(m);
+
+		auto start = std::chrono::high_resolution_clock::now();
 		temp = read_sensor(sensor_path);
+		auto end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::milli> elapsed = end - start;
+		std::cout << "Device read took " << elapsed.count() << "ms.  Adjusting sleep time.\n";
+
+		if(temp == -1) {
+			std::cout << "Error reading sensor.\n";	
+			exit(-1);
+		}
+
+		m.unlock();
 		cv.notify_one();
-		std::this_thread::sleep_for(std::chrono::seconds(5));
+		std::this_thread::sleep_for(read_frequency - elapsed);
 	}
 }
 
@@ -48,12 +63,12 @@ int main(int argc, char* argv[]) {
 	std::thread poll(poll_sensor);
 	int current_temp = 0;
 
-	for(;;) {
+	while(1) {
 		std::unique_lock<std::mutex> lock(m);
 		cv.wait(lock);
-		{	
+		{
 			current_temp = temp;
 		}
-		std::cout << "read temp: " << current_temp << "\n";
-	}	
+		std::cout << "read temp: " << (current_temp / 1000.0) << "\n";
+	}
 }
